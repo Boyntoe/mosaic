ee8468ec1538e9d216cbf14079f794eae65e4edd
Emily Boynton <eoboynton@gmail.com>
2024-03-22 16:36:53 -0700

    debug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
       <GoogleTagManager gtmId="GTM-PWDMVDDR" />
+      <Head />
       <body>
         <Main />
         <NextScript />
